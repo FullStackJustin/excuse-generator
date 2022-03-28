cf02d668cc9d213f48cea07287257c506ba5f6fe
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-03-28 19:41:46 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function excuse() {
+  var part1 = ["homie", "sister", "grandma"];
+  var verb = ["sabotaged", "stole", "destroyed"];
+  var noun = ["homework", "pizza", "phone"];
+
+  let who = part1[Math.round(Math.random() * (part1.length - 1))];
+  let did = verb[Math.round(Math.random() * (verb.length - 1))];
+  let what = noun[Math.round(Math.random() * (noun.length - 1))];
+
+  document.getElementById("generator").innerHTML =
+    "My " + who + " " + did + " my " + what + ".";
 };
